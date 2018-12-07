c9469125848be7d59ff1bc7319618e6310dd59b4
Jesse <jesse@mondorobot.com>
2018-12-07 16:29:34 -0700

    rename install_composer to install_dependencies

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+if ! [ -x "$(command -v composer)" ]; then
+  curl -sS "https://getcomposer.org/installer" | php
+
+  mv "composer.phar" "/usr/local/bin/composer"
+  ln -s "/usr/local/bin/composer" "/usr/bin/composer"
+fi
+
+if ! [ -x "$(command -v composer)" ]; then
+  curl -o "/bin/wp" "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
+  chmod +x "/bin/wp"
+  wp package install "aaemnnosttv/wp-cli-dotenv-command:^1.0" --allow-root
+fi
