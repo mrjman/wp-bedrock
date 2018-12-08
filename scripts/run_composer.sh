56e170dd8165619aab5a008be5c8ddde6a8b1cd0
Jesse <jesse@mondorobot.com>
2018-12-08 12:49:51 -0700

    update run_composer.sh script

--- scripts/run_composer.sh
+++ scripts/run_composer.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -xe
+site_directory="/var/www/wordpress/coachella"
 
-COMPOSER_HOME="/var/cache/composer" composer install -d "/var/www/wordpress/coachella"
+cd "$site_directory"
+cp ".env.example" ".env"
+
+# get acf pro key from ssm and remove quotes from the end of the string
+acf_pro_key=$(aws ssm get-parameters --region us-east-1 --names coa.wp.acf_pro_key --with-decryption --query Parameters[0].Value)
+acf_pro_key=`echo $acf_pro_key | sed -e 's/^"//' -e 's/"$//'`
+
+wp dotenv set ACF_PRO_KEY $acf_pro_key --quote-double --allow-root
+
+COMPOSER_HOME="/var/cache/composer" composer install
