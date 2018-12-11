8f56d3ff0ed36908ce2edb8dc7a9a7adf79cb19c
Jesse <jesse@mondorobot.com>
2018-12-11 11:27:17 -0700

    update after_install script to delete current site before replacing with new site

--- scripts/after_install_root.sh
+++ scripts/after_install_root.sh
@@ -3,6 +3,10 @@
 tmp_directory="/tmp/coachella"
 site_directory="/var/www/wordpress/coachella"
 
+if [ -d "$site_directory" ]; then
+  rm -rf "$site_directory"
+fi
+
 mv "$tmp_directory" "$site_directory"
 chown -R apache:apache "$site_directory"
 find "$site_directory" -type d -exec chmod 750 {} \;
